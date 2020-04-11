1ba5c0bb5a51be5a23d599f056ad42907d562fb3
Alessandro Corra <alessandro.corra@lovehoney.co.uk>
2020-04-11 17:37:09 +0100

    row of brick randomly generated added with flag to render them or not

--- arkanoid.js
+++ arkanoid.js
@@ -4,12 +4,11 @@ const RED = "#fc140f";
 const DARK_GREY = "#403f3f";
 const YELLOW = "#fff275";
 const GREEN = "#04e762";
-
-let canvas, canvasContext;
-let framePerSecond = 30;
+const ORANGE = "#ff6b35";
+const BLUE = "#185ced";
 
 const CANVAS_WIDTH = 800;
-const CANVAS_HEIGHT = 600;
+const CANVAS_HEIGHT = 600; 
 
 const PADDLE_WIDTH = 100;
 const PADDLE_THICKNESS = 10;
@@ -19,6 +18,14 @@ const GAP = 10;
 const BALL_SIZE = 10;
 const BALL_SPEED_Y = 7;
 
+const BRICK_W = 100;
+const BRICK_H = 50;
+const BRICK_COUNT = 8;
+const BRICK_GAP = 2;
+
+let canvas, canvasContext;
+let framePerSecond = 30;
+
 let ballX = 75;
 let ballSpeedX = 5;
 let ballY = 75;
@@ -27,6 +34,11 @@ let ballSpeedY = BALL_SPEED_Y;
 let paddleX = CANVAS_WIDTH / 2;
 let paddleY = CANVAS_HEIGHT - PADDLE_THICKNESS - GAP;
 
+let mouseX;
+let mouseY;
+
+let brickGrid = new Array;
+
 const updateMousePosition = (e) => {
 
 	// to get the position of the mouse in the canvas,
@@ -34,11 +46,21 @@ const updateMousePosition = (e) => {
 	let rect = canvas.getBoundingClientRect();
 	let root = document.documentElement;
 
-	let mouseX = e.clientX - rect.left - root.scrollLeft;
-	// let mouseY = e.clientY - rect.top - root.scrollTop;	// for now we don't need to move the paddle on Y axis
+	mouseX = e.clientX - rect.left - root.scrollLeft;
+	mouseY = e.clientY - rect.top - root.scrollTop;
 
 	paddleX = mouseX - (PADDLE_WIDTH / 2);		// PADDLE_WIDTH / 2 is to place the mouse in the center of the paddle
 	// paddleY = mouseY;	// for now we don't need to move the paddle on Y axis
+}
+
+const brickReset = () => {
+	for (let i = 0; i < BRICK_COUNT; i++) {
+		if (Math.random() < 0.5) {
+			brickGrid[i] = true;
+		} else {
+			brickGrid[i] = false;
+		}
+	}
 }
 
 // ============================== *** CORE FUNCTION *** ==============================
@@ -51,6 +73,8 @@ window.onload = () => {
 	setInterval(updateAll, 1000 / framePerSecond);
 
 	canvas.addEventListener('mousemove', updateMousePosition);
+
+	brickReset();
 }
 // ===================================================================================
 
@@ -107,6 +131,8 @@ const drawAll = () => {
 	drawRect(0, 0, canvas.width, canvas.height, BLACK);
 	drawBall(ballX, ballY, BALL_SIZE, RED);
 	drawRect(paddleX, paddleY, PADDLE_WIDTH, PADDLE_THICKNESS, WHITE);
+	colorText(`X:${mouseX} ; Y:${mouseY}`, mouseX, mouseY, YELLOW);
+	drawBricks();
 }
 
 const drawRect = (topLeftX, topLeftY, rectWidth, rectHeight, color) => {
@@ -121,7 +147,20 @@ const drawBall = (centerX, centerY, radius, color) => {
 	canvasContext.fill();
 }
 
+const drawBricks = () => {
+	for (let i = 0; i < BRICK_COUNT; i++) {
+		if (brickGrid[i]) {
+			drawRect(BRICK_W * i, 0, BRICK_W - BRICK_GAP, BRICK_H, BLUE);
+		}
+	}
+}
+
 const resetBall = () => {
 	ballX = canvas.width / 2;
 	ballY = canvas.height / 2;
+}
+
+const colorText = (textString, textX, textY, color) => {
+	canvasContext.fillStyle = color;
+	canvasContext.fillText(textString, textX, textY);
 }
